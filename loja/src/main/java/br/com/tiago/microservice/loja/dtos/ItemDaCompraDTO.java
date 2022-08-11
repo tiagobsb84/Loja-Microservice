aaebe52c2a320c08d50aa1ba601375639bbd177f
Tiago <tiagobsb84@gmail.com>
2022-08-11 10:46:23 -0300

    Primeiro microservice loja

---
+++ loja/src/main/java/br/com/tiago/microservice/loja/dtos/ItemDaCompraDTO.java
@@ -0,0 +1,22 @@
+package br.com.tiago.microservice.loja.dtos;
+
+public class ItemDaCompraDTO {
+
+	private Long id;
+	private int quantidade;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+}
